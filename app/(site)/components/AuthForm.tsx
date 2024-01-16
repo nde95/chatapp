8e5fd53e3a192ab75f645651dfeb214d2182cb10
nde95 <ndennison268@gmail.com>
2024-01-16 13:32:33 -0500

    working credentials sign in

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -8,6 +8,7 @@ import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -46,7 +47,22 @@ const AuthForm = () => {
     }
 
     if (variant === "LOGIN") {
-      // NextAuth login logic
+      signIn("credentials", {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid Login.");
+          }
+
+          if (callback?.ok && !callback?.error) {
+            toast.success("Logged in successfully!");
+          }
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
     }
   };
 
