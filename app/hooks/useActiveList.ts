ec8299c7a0b5c0e32f4fa3edbe8bba41e86cd94c
nde95 <ndennison268@gmail.com>
2024-01-20 16:56:34 -0500

    hooks to grab online status

---
+++ app/hooks/useActiveList.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+interface ActiveUserStore {
+  members: string[];
+  add: (id: string) => void;
+  remove: (id: string) => void;
+  set: (ids: string[]) => void;
+}
+
+const useActiveUsers = create<ActiveUserStore>((set) => ({
+  members: [],
+  add: (id) => set((state) => ({ members: [...state.members, id] })),
+  remove: (id) =>
+    set((state) => ({
+      members: state.members.filter((memberId) => memberId !== id),
+    })),
+  set: (ids) => set({ members: ids }),
+}));
+
+export default useActiveUsers;
